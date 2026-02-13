5d8cb1e6f5f2e72e9936a04af9ff1bfd799f5e61
oaao <oliver+github@abolish.golf>
2026-02-12 18:02:12 -0700

    rev [04 - react intro]: ex01 - assemble all components, oof ugly!

--- 04 - react + front-end frameworks/examples/01 - react + vite + tailwind/instructor/src/App.jsx
+++ 04 - react + front-end frameworks/examples/01 - react + vite + tailwind/instructor/src/App.jsx
@@ -2,10 +2,21 @@ import './App.css'
 
 // components
 import Header from './components/Header';
+import Details from './components/Details';
+import Filters from './components/Filters';
+import Results from './components/Results';
+
 
 function App() {
 
-    return <Header />;
+    return (
+      <>
+        <Header />
+        <Filters />
+        <Results />
+        <Details />
+      </>
+    );
 }
 
 export default App
